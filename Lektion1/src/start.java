1320b35fe9b82d7adb4ee3276278700c62c0a1cf
galandr80 <galandr80@andreasljansmbp.fritz.box>
2015-12-21 18:15:29 +0100

    add hallo

---
+++ Lektion1/src/start.java
@@ -0,0 +1,13 @@
+
+public class start {
+       
+	public static void main(String[] args) {
+		int ura;
+		summa s= new summa();
+	    ura =s.sum(10, 5);
+	    System.out.println(ura);
+        System.out.println("Hallo");
+        
+	}
+
+}
